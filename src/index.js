9d969d3d920b7e78e17d95c1ce7f4af43747104a
ms_nizhevelev <ms_nizhevelev@controlsystems.ru>
2026-05-09 17:22:50 +0300

    OrbitControls

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 // Scene
 const scene = new THREE.Scene();
@@ -17,6 +18,10 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// OrbitControls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
 // Cube
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
@@ -30,6 +35,8 @@ function animate() {
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
 
+  controls.update();
+  
   renderer.render(scene, camera);
 }
 
