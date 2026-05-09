c1ab89cd5b87da25253382029943d8be328c436d
ms_nizhevelev <ms_nizhevelev@controlsystems.ru>
2026-05-09 17:35:30 +0300

    Lighting and Shadows

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 // Scene
 const scene = new THREE.Scene();
 
+// Light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
+ambientLight.intensity = 0.6;
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(5, 5, 5);
+directionalLight.castShadow = true;
+scene.add(directionalLight);
+
 // Camera
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -15,6 +25,8 @@ camera.position.z = 5;
 
 // Renderer
 const renderer = new THREE.WebGLRenderer();
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -24,8 +36,9 @@ controls.enableDamping = true;
 
 // Cube
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 })
 const cube = new THREE.Mesh(geometry, material);
+cube.castShadow = true;
 scene.add(cube);
 
 // Animation
@@ -36,7 +49,7 @@ function animate() {
   cube.rotation.y += 0.01;
 
   controls.update();
-  
+
   renderer.render(scene, camera);
 }
 
