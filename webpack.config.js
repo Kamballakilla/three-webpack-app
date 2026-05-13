dedfa4bd8fac5ed0973d864fe18363db885ceea5
ms_nizhevelev <ms_nizhevelev@controlsystems.ru>
2026-05-13 17:08:01 +0300

    fix html build

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,42 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-
-    // для GitHub Pages:
     publicPath: "/three-webpack-app/",
   },
+
   devServer: {
     static: "./dist",
     open: true,
     hot: true,
   },
+
   module: {
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader",
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
       },
-    },
-    {
-      test: /\.css$/i,
-      use: ["style-loader", "css-loader"],
-    },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
   ],
-},
-  
 };
